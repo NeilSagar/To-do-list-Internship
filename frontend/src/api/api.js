510b402487d4ec1a4eece96d57e73071c0e68169
Neil Sagar Sahu <neilsagar55555@gmail.com>
2023-06-12 13:26:34 +0530

    made api in front-end for created,delete and view tasks

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL="";
+const URL="http://localhost:5000";
+
 export const getAllTasks=async()=>{
     try {
         const response=await axios.get(URL+"/getAllTasks");
@@ -8,5 +9,25 @@ export const getAllTasks=async()=>{
     } catch (error) {
         console.log("Error while fetching tasks:",error);
         return error;
+    }
+}
+
+export const addTask=async(data)=>{
+    try {
+        const response=await axios.post(URL+"/addTask",data);
+        return response;
+    } catch (error) {
+        console.log("Error while adding task:",error);
+        return error;
+    }
+}
+
+export const deleteTask=async(task)=>{
+    const taskname=task.task_name;
+    console.log(task);
+    try {
+        const response=await axios.delete(URL+"/deleteTask/"+taskname);
+    } catch (error) {
+        console.log("Error while deleting task:",error);
     }
 }
