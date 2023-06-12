4604bdc8228452a54981bf0e54c4be8e977daf3c
Neil Sagar Sahu <neilsagar55555@gmail.com>
2023-06-12 13:27:45 +0530

    made changes in taskPage to accomodate functionality of view,create,delete of tasks

--- frontend/src/components/taskPage.jsx
+++ frontend/src/components/taskPage.jsx
@@ -1,23 +1,60 @@
 import { useEffect, useState } from "react";
 import React from "react";
 
+import { getAllTasks,addTask, deleteTask } from "../api/api";
+
+
+
 function TaskPage(){
-    const [tasks,setTasks]=useState(["Exercise","meditation","DSA revision"]);
+    const [tasks,setTasks]=useState([]);
+    const [newTask,setNewTask]=useState("");
+    async function handleAdd(){
+        if(newTask==="")return;
+        const task={task:newTask};
+        await addTask(task);
+    }
+    async function getAllInfo(){
+        const response=await getAllTasks();
+        if(response.status===201){
+            const res_tasks=response.data;
+            setTasks(res_tasks);
+        }
+    }
+    function handleInputChange(event){
+        const value=event.target.value;
+        setNewTask(value);
+    }
+
+    async function handleDelete(event){
+        const task_index=event.target.id;
+        const task_name=tasks[task_index];
+        
+        await deleteTask(task_name);
+    }
+
+    useEffect(()=>{
+        getAllInfo();
+    },[tasks]);
 
 return(
         <>
             <h1>Today</h1>
             <div className="task-sec">
                 {
-                    tasks.length==0?<>Looks like you don't have any task today</>:
+                    tasks.length===0?<>Looks like you don't have any task today</>:
                     tasks.map((task,i)=>{
-                    return (<p key={i}>
-                        {task}
-                    </p>)
+                    return (
+                        <div key={i}>
+
+                        <p style={{display:"inline-block"}} id={i} > {task.task_name}</p>
+                        <button id={i} onClick={handleDelete}>delete</button>
+
+                        </div>
+                        )
                 })}
             </div>
-            <input type="text"/>
-            <button>add</button>
+            <input type="text" name="newTask" value={newTask} onChange={handleInputChange}/>
+            <button onClick={handleAdd}>add</button>
         </>
     );
 }
