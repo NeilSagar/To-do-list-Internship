48b42b5a7f3e8f0b4a9162e8dc0f15ca47a11616
Neil Sagar Sahu <neilsagar55555@gmail.com>
2023-06-12 13:46:52 +0530

    removed unused imports

--- backend/schemas/taskSchema.js
+++ backend/schemas/taskSchema.js
@@ -1,6 +1,3 @@
-import mysql from "mysql";
-
-import { db } from "../db/connection.js";
 
 const sql_createTask_table="CREATE TABLE tasks(id int AUTO_INCREMENT, task_name VARCHAR(50), PRIMARY KEY(id))";
 
